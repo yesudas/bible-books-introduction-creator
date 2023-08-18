4e6560293fdda65ea5fa694404444a8fb62e8b33
Yesudas S <yesudas.rs@gmail.com>
2023-08-18 12:02:48 +0530

    adding files for the first time

---
+++ src/in/wordofgod/bible/books/introduction/creator/BibleBooksIntroductionCreator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package in.wordofgod.bible.books.introduction.creator;
+
+/**
+ * 
+ */
+public class BibleBooksIntroductionCreator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
